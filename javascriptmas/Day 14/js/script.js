a7af8601497d22cb125d51d7d4a4d041036d9d83
brodiewebdt <81633541+brodiewebdt@users.noreply.github.com>
2021-12-14 14:16:19 -0500

    fixed JS code for accessing array

--- javascriptmas/Day 14/js/script.js
+++ javascriptmas/Day 14/js/script.js
@@ -1,30 +1,41 @@
-
-
 const elf = document.getElementById("elf")
 const btn = document.getElementById("btn")
 const btnTool = document.getElementById("btn-tool")
 
-const tools = ['🔨', '🧃', '🪓', '☕', '🍗', '⛏️', '🔧', '🍕', '🗡️', '🥪', '🏹', '🍦']
+const tools = [
+  "🔨",
+  "🧃",
+  "🪓",
+  "☕",
+  "🍗",
+  "⛏️",
+  "🔧",
+  "🍕",
+  "🗡️",
+  "🥪",
+  "🏹",
+  "🍦",
+]
 
 btn.addEventListener("click", duplicateElf)
 
-function duplicateElf(){
-    // Task:
-   // - Write a function to duplicate the elf when the button is clicked.
-   // - See index.css for optional styling challenges.
+function duplicateElf() {
+  // Task:
+  // - Write a function to duplicate the elf when the button is clicked.
+  // - See index.css for optional styling challenges.
 }
 
 // Stretch goals:
 // - Write a function to give the elves some tools, or a cup of tea!
 // - Limit the total number of elves to 100.
-btn.addEventListener('click', function() {
-    elf.textContent += '🧝'
-    // elf.textContent += tools[0]
+btn.addEventListener("click", function () {
+  elf.textContent += "🧝"
+  // elf.textContent += tools[0]
 })
 
-btnTool.addEventListener('click', function() {
-    // elf.textContent += '🧝'
-    // elf.textContent += tools[1]
-    const num = Math.floor(Math.random() * 12) + 1
-    elf.textContent += tools[num]
+btnTool.addEventListener("click", function () {
+  // elf.textContent += '🧝'
+  // elf.textContent += tools[1]
+  const num = Math.floor(Math.random() * tools.length)
+  elf.textContent += tools[num]
 })
